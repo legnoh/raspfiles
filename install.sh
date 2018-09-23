6eff01737d8a82fe5c0b99230c173a5a181b9912
Ryoma Kai <legknmj@icloud.com>
2018-09-23 16:41:43 +0900

    add soma blinds service, add core libs for python

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ ndenv global v10.11.0
 ndenv rehash
 
 ## install python
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev
 anyenv install pyenv
 pyenv install 3.7.0
 pyenv global 3.7.0
@@ -30,10 +33,9 @@ npm install -g https://github.com/paolotremadio/homebridge-minimal-http-blinds
 mkdir ~/.homebridge
 cp conf/config.json ~/.homebridge/config.json
 
-# prepare soma smart blinds
+# prepare SOMA smart blinds
 git clone https://github.com/paolotremadio/SOMA-Smart-Shades-HTTP-API.git ~/webshades
-sudo hciconfig hci0 up
-sudo hcitool lescan
+cd webshades
 sudo pip install webshades.py
 
 # execute in daemon
@@ -41,10 +43,13 @@ sudo useradd --system homebridge
 sudo mkdir /var/homebridge
 sudo chown homebridge:homebridge /var/homebridge/
 sudo cp conf/etc-default.conf /etc/default/homebridge
-sudo cp conf/service.ini /etc/systemd/system/homebridge.service
+sudo cp conf/service-homebridge.ini /etc/systemd/system/homebridge.service
+sudo cp conf/service-soma-blinds.ini /etc/systemd/system/somablinds.service
 sudo cp ~/.homebridge/config.json /var/homebridge/
 sudo cp -r ~/.homebridge/persist /var/homebridge
 sudo chmod -R 0777 /var/homebridge
 sudo systemctl daemon-reload
+sudo systemctl enable somablinds
+sudo systemctl start somablinds
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
