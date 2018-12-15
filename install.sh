5f4a2c09bec1432aaa62eb9fe18d4219aa0d91e5
Ryoma Kai <legknmj@icloud.com>
2018-12-15 18:36:02 +0900

    some changes... - remove cecclient - update to 10.14.2 - config.json to erb file

--- install.sh
+++ install.sh
@@ -11,12 +11,12 @@ exec $SHELL -l
 ## install node
 anyenv install ndenv
 exec $SHELL -l
-ndenv install v10.14.1
-ndenv global v10.14.1
+ndenv install v10.14.2
+ndenv global v10.14.2
 ndenv rehash
 
-## update pip
-sudo apt install python-pip
+## update pip and install direnv
+sudo apt install python-pip direnv
 
 # install homebridge
 npm install -g homebridge
@@ -24,17 +24,10 @@ npm install -g homebridge-tado-ac
 npm install -g homebridge-http-switch
 npm install -g https://github.com/paolotremadio/homebridge-minimal-http-blinds
 
-# install fix branch versions homebridge-cec-accessory
-cd ~/.anyenv/envs/ndenv/versions/v10.14.1/lib/node_modules
-git clone https://github.com/jbree/homebridge-cec-accessory.git
-cd homebridge-cec-accessory
-git checkout -b branch-timeout-fix origin/branch-timeout-fix
-npm install
-cd
-
 # make config file
 mkdir ~/.homebridge
-cp ~/raspfiles/conf/config.json ~/.homebridge/config.json
+direnv allow # get envrc file!
+erb ~/raspfiles/conf/config.json.erb > ~/.homebridge/config.json
 
 # prepare SOMA smart blinds
 git clone https://github.com/paolotremadio/SOMA-Smart-Shades-HTTP-API.git ~/webshades
@@ -44,20 +37,13 @@ sudo apt-get install python-webpy
 sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hcitool`
 sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hciconfig`
 
-# change permission of homebridge to cec
-sudo apt-get install cec-utils
-echo hdmi_ignore_cec_init=1 | sudo tee -a /boot/config.txt
-
 # execute in daemon
 sudo useradd --system homebridge
-sudo usermod -a -G video homebridge
-sudo chown -R homebridge:homebridge /usr/bin/cec-client
 sudo mkdir /var/homebridge
 sudo chown homebridge:homebridge /var/homebridge/
 sudo cp ~/raspfiles/conf/etc-default.conf /etc/default/homebridge
 sudo cp ~/raspfiles/conf/service-homebridge.ini /etc/systemd/system/homebridge.service
 sudo cp ~/raspfiles/conf/service-soma-blinds.ini /etc/systemd/system/somablinds.service
-sudo cp ~/raspfiles/conf/service-cec-client.ini /etc/systemd/system/cecclient.service
 sudo cp ~/.homebridge/config.json /var/homebridge/
 sudo mkdir -r /var/homebridge/persist
 sudo chown -R homebridge:homebridge ~/webshades
@@ -66,8 +52,6 @@ sudo chmod -R 0777 /var/homebridge
 sudo systemctl daemon-reload
 sudo systemctl enable somablinds
 sudo systemctl start somablinds
-sudo systemctl enable cecclient
-sudo systemctl start cecclient
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
 
