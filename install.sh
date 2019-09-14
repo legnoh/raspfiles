4a8dbd21d6fcdcaee1683dbf78915850570c2df2
Ryoma Kai <rkai@yahoo-corp.jp>
2019-09-14 20:39:19 +0900

    add hc-opengate settings

--- install.sh
+++ install.sh
@@ -21,9 +21,6 @@ echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 
 # install homebridge
 npm install -g homebridge
-npm install -g homebridge-http-switch
-npm install -g homebridge-cmdswitch2
-npm install -g homebridge-tado-ac
 npm install -g paolotremadio/homebridge-minimal-http-blinds
 npm install -g legnoh/homebridge-daikin-air-purifier
 
@@ -39,8 +36,10 @@ git clone https://github.com/paolotremadio/SOMA-Smart-Shades-HTTP-API.git ~/webs
 sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hcitool`
 sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hciconfig`
 
-# cec setting
-echo hdmi_ignore_cec_init=1 | sudo tee -a /boot/config.txt
+# install hc-http-switch
+wget https://github.com/legnoh/hc-http-switch/releases/download/v1.0.0/hc-http-switch_v1.0.0_linux_arm7.zip
+unzip hc-http-switch_v1.0.0_linux_arm7.zip
+mv hc-http-switch /usr/local/bin/
 
 # execute in daemon
 sudo useradd --system homebridge
@@ -49,12 +48,15 @@ sudo chown homebridge:homebridge /var/homebridge/
 sudo cp ~/raspfiles/conf/etc-default.conf /etc/default/homebridge
 sudo cp ~/raspfiles/conf/service-homebridge.ini /etc/systemd/system/homebridge.service
 sudo cp ~/raspfiles/conf/service-soma-blinds.ini /etc/systemd/system/somablinds.service
+sudo cp ~/raspfiles/conf/service-hc-opengate.ini /etc/systemd/system/opengate.service
 sudo cp ~/.homebridge/config.json /var/homebridge/
 sudo mkdir -r /var/homebridge/persist
 sudo chown -R homebridge:homebridge ~/webshades
 exec $SHELL -l
 sudo chmod -R 0777 /var/homebridge
 sudo systemctl daemon-reload
+sudo systemctl enable opengate
+sudo systemctl start opengate
 sudo systemctl enable somablinds
 sudo systemctl start somablinds
 sudo systemctl enable homebridge
