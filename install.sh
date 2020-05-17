927cd8822adf8ab10725e2784582b27532c60e75
Ryoma Kai <legknmj@icloud.com>
2020-05-17 15:56:03 +0000

    add soma files

--- install.sh
+++ install.sh
@@ -11,28 +11,37 @@ exec $SHELL -l
 ## install node
 anyenv install nodenv
 exec $SHELL -l
-nodenv install v10.15.1
-nodenv global v10.15.1
+nodenv install 12.16.3
+nodenv global 12.16.3
 nodenv rehash
 
-## update pip and install direnv
+# install ruby
+sudo apt install ruby
+
+## install direnv
 sudo apt install direnv
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 
 # install homebridge
 npm install -g homebridge
-npm install -g legnoh/homebridge-daikin-air-purifier
+npm install -g homebridge-sesame
+npm install -g paolotremadio/homebridge-minimal-http-blinds
 
 # make config file
+git clone https://github.com/legnoh/raspfiles.git ~/raspfiles && cd raspfiles
 mkdir ~/.homebridge
 direnv allow # get envrc file!
 erb ~/raspfiles/conf/config.json.erb > ~/.homebridge/config.json
 
-# install hc-http-switch
-wget https://github.com/legnoh/hc-http-switch/releases/download/v1.0.8/hc-http-switch_1.0.8_linux_armv7.tar.gz
-tar zxf hc-http-switch_1.0.8_linux_armv7.tar.gz
-sudo chown root:staff hc-http-switch
-sudo mv hc-http-switch /usr/local/bin/
+# prepare SOMA smart blinds
+sudo apt install python3-pip
+
+git clone https://github.com/paolotremadio/SOMA-Smart-Shades-HTTP-API.git ~/webshades && cd ~/webshades
+pip3 install web.py
+
+# https://bbs.archlinux.org/viewtopic.php?id=215080
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hcitool`
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hciconfig`
 
 # execute in daemon
 sudo useradd --system homebridge
@@ -40,14 +49,15 @@ sudo mkdir /var/homebridge
 sudo chown homebridge:homebridge /var/homebridge/
 sudo cp ~/raspfiles/conf/etc-default.conf /etc/default/homebridge
 sudo cp ~/raspfiles/conf/service-homebridge.ini /etc/systemd/system/homebridge.service
-sudo cp ~/raspfiles/conf/service-hc-opengate.ini /etc/systemd/system/opengate.service
+sudo cp ~/raspfiles/conf/service-soma-blinds.ini /etc/systemd/system/somablinds.service
 sudo cp ~/.homebridge/config.json /var/homebridge/
 sudo mkdir -r /var/homebridge/persist
+sudo chown -R homebridge:homebridge ~/webshades
 exec $SHELL -l
 sudo chmod -R 0777 /var/homebridge
 sudo systemctl daemon-reload
-sudo systemctl enable opengate
-sudo systemctl start opengate
+sudo systemctl enable somablinds
+sudo systemctl start somablinds
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
 
